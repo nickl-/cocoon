2811f4234e437db886e3611f71373fe02b242ca2
nickl- <github@jigsoft.co.za>
2013-10-22 17:35:32 +0200

    Template rework.
    Reusable and interchangeable views.
    Layout page
    Generate all fields
    Generate grids for all has_many relations

--- lib/generators/rails/cocoon_model/templates/model.rb
+++ lib/generators/rails/cocoon_model/templates/model.rb
@@ -1,7 +1,13 @@
 <% module_namespacing do -%>
 class <%= class_name %> < <%= parent_class_name.classify %>
+<% if scope_attributes.include? 'payload' -%>
+  include UploadDocument
+<% end -%>
 <% attributes.select(&:reference?).each do |attribute| -%>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %>
+<% end -%>
+<% attributes.select(&:has_and_belongs_to_many?).each do |attribute| -%>
+has_and_belongs_to_many :<%= attribute.plural_name %>
 <% end -%>
 <% if attributes.any?(&:password_digest?) -%>
   has_secure_password
--- lib/generators/rails/cocoon_model/templates/nested_serializer.rb
+++ lib/generators/rails/cocoon_model/templates/nested_serializer.rb
@@ -1,7 +1,7 @@
 <% module_namespacing do -%>
 class <%= class_name %>Serializer < ActiveModel::Serializer
 <% attributes_names.map(&:inspect).each do |att| -%>
-  attribute <%= att %><%= att =~ /_/ ? ", key: #{att.camelize(:lower)}" : '' %>
+  attribute <%= att %><%= att =~ /_/ ? "#, key: #{att.camelize(:lower)}" : '' %>
 <% end -%>
 end
 <% end -%>
