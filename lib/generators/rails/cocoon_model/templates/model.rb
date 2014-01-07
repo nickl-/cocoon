9def38e2b4fecf1c0ce5b8d2f684219f34c8129e
nickl- <github@jigsoft.co.za>
2014-01-07 10:38:51 +0200

    Fixes habtm associations

--- lib/generators/rails/cocoon_model/templates/model.rb
+++ lib/generators/rails/cocoon_model/templates/model.rb
@@ -7,7 +7,8 @@ class <%= class_name %> < <%= parent_class_name.classify %>
   belongs_to :<%= attribute.name %><%= ', polymorphic: true' if attribute.polymorphic? %>
 <% end -%>
 <% attributes.select(&:has_and_belongs_to_many?).each do |attribute| -%>
-has_and_belongs_to_many :<%= attribute.plural_name %>
+  has_and_belongs_to_many :<%= attribute.plural_name %>
+  accepts_nested_attributes_for :<%= attribute.plural_name %>, allow_destroy: true, update_only: true
 <% end -%>
 <% if attributes.any?(&:password_digest?) -%>
   has_secure_password
